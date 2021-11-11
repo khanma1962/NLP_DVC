b7f2ea3ccc18d7c3d13562fef3a1d79f82dc70ee
Moe Khan <m_a_khan_usa@yahoo.com>
2021-11-12 00:15:19 +0530

    feature for save_marix

---
+++ src/utils/featurize.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import os
+import logging
+import joblib
+import scipy.sparse as sparse
+
+def save_matrix(df, matrix, out_path):
+    id_matrix = sparse.csc_matrix(df.id.astype(np.int64)).T 
+    label_matrix = sparse.csc_matrix(df.label.astype(np.int64)).T 
+
+    result = sparse.hstack([id_matrix, label_matrix, matrix], format="csr")
+
+    msg = f"output matrix {out_path} of size {result.shape} and data type {result.dtype}"
+    joblib.dump(result, out_path)
+
+    
